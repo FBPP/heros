ffdbe30608e6a32698ecc309074eda225c0c944a
Pengpeng <pengcipaci@163.com>
2023-05-01 18:59:11 +0800

    aas

--- src/app/routes/ntp-desktop/components/cursor/cursor.component.ts
+++ src/app/routes/ntp-desktop/components/cursor/cursor.component.ts
@@ -21,7 +21,7 @@ export class CursorComponent implements OnInit {
   private x$ = new BehaviorSubject<number>(0);
   private y$ = new BehaviorSubject<number>(0);
   
-  constructor(hostRef: ElementRef<HTMLDivElement>) { 
+  constructor(private hostRef: ElementRef<HTMLDivElement>) { 
     combineLatest([this.x$, this.y$]).subscribe(([x, y]) => {
       const width = hostRef.nativeElement.clientWidth;
       const height = hostRef.nativeElement.clientHeight
@@ -33,7 +33,56 @@ export class CursorComponent implements OnInit {
   }
 
   explosion() {
-    
+    const tl = gsap.timeline();
+    tl.set(this.hostRef.nativeElement, {
+      x: this.x$.value - this.hostRef.nativeElement.clientWidth / 2,
+      y: this.y$.value - this.hostRef.nativeElement.clientHeight / 2,
+      scale: 0.3,
+      boxShadow: `0 0 0px 0px #9ab3f5`,
+      onUpdate: () => {
+        gsap.set(this.hostRef.nativeElement, {
+          x: this.x$.value - this.hostRef.nativeElement.clientWidth / 2,
+          y: this.y$.value - this.hostRef.nativeElement.clientHeight / 2,
+        })
+      }
+    }).to(this.hostRef.nativeElement, {
+      scale: 1,
+      opacity: 0.7,
+      x: this.x$.value - this.hostRef.nativeElement.clientWidth / 2,
+      y: this.y$.value - this.hostRef.nativeElement.clientHeight / 2,
+      boxShadow: `0 0 10px 8px #9ab3f5`,
+      duration: 0.1,
+      onUpdate: () => {
+        gsap.set(this.hostRef.nativeElement, {
+          x: this.x$.value - this.hostRef.nativeElement.clientWidth / 2,
+          y: this.y$.value - this.hostRef.nativeElement.clientHeight / 2,
+        })
+      }
+    }).to(this.hostRef.nativeElement, {
+      scale: 2,
+      opacity: 0,
+      boxShadow: `0 0 0 0 #9ab3f5`,
+      x: this.x$.value - this.hostRef.nativeElement.clientWidth / 2,
+      y: this.y$.value - this.hostRef.nativeElement.clientHeight / 2,
+      onUpdate: () => {
+        gsap.set(this.hostRef.nativeElement, {
+          x: this.x$.value - this.hostRef.nativeElement.clientWidth / 2,
+          y: this.y$.value - this.hostRef.nativeElement.clientHeight / 2,
+        })
+      }
+    }).to(this.hostRef.nativeElement, {
+      scale: 1,
+      opacity: 1,
+      x: this.x$.value - this.hostRef.nativeElement.clientWidth / 2,
+      y: this.y$.value - this.hostRef.nativeElement.clientHeight / 2,
+      duration: 0.1,
+      onUpdate: () => {
+        gsap.set(this.hostRef.nativeElement, {
+          x: this.x$.value - this.hostRef.nativeElement.clientWidth / 2,
+          y: this.y$.value - this.hostRef.nativeElement.clientHeight / 2,
+        })
+      }
+    }) 
   }
 
 }
--- src/app/routes/ntp-desktop/ntp-desktop.module.ts
+++ src/app/routes/ntp-desktop/ntp-desktop.module.ts
@@ -5,13 +5,15 @@ import { NTPDesktopRoutingModule } from './ntp-desktop-routing.module';
 import { NtpDesktopIndexComponent } from './ntp-desktop-index/ntp-desktop-index.component';
 import { CursorComponent } from './components/cursor/cursor.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { BackgroundComponent } from './components/background/background.component';
 
 
 @NgModule({
   declarations: [
     NtpDesktopIndexComponent,
     CursorComponent,
-    MenuComponent
+    MenuComponent,
+    BackgroundComponent
   ],
   imports: [
     CommonModule,
